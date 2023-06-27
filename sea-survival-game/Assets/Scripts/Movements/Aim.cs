9b48ef77c2eb48868914487c779b74ab8abd73a0
TheMC2005 <erdelyi.nimrod05@gmail.com>
2023-06-27 23:38:29 +0200

    player updated

---
+++ sea-survival-game/Assets/Scripts/Movements/Aim.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Aim : MonoBehaviour
+{
+    public Camera cam;
+    private Rigidbody2D rb;
+    GameObject pl;
+    Vector2 mPos;
+    Vector2 pos;
+    float r;
+
+    void Start()
+    {
+        pl = GameObject.Find("player");
+    }
+
+    void Update()
+    {
+        mPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        pos = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lDir = mPos - pos;
+        float d = Mathf.Atan2(lDir.y, lDir.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0, 0, d);
+        r = transform.localEulerAngles.z;
+    }
+}

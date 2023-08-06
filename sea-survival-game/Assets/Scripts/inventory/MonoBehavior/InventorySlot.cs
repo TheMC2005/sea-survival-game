78bb52824920720cde8c3b080e7492059683b229
TheMC2005 <erdelyi.nimrod05@gmail.com>
2023-08-06 18:55:02 +0200

    inventory 6

--- sea-survival-game/Assets/Scripts/inventory/MonoBehavior/InventorySlot.cs
+++ sea-survival-game/Assets/Scripts/inventory/MonoBehavior/InventorySlot.cs
@@ -8,9 +8,10 @@ public class InventorySlot : MonoBehaviour
 {
     [SerializeField] TMP_Text text;
     [SerializeField] Image icon;
-
+    Slot slot;
     public void Set(Slot slot)
     {
+        this.slot=slot;
         icon.sprite = slot.item.sprite;
         if ((slot.count == 0) || (slot.item.maxq == 1))
         {

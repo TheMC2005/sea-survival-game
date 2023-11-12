aec8fca338c7529b00755cc89295e237d365e788
TheMC2005 <erdelyi.nimrod05@gmail.com>
2023-11-12 01:58:34 +0100

    buildings1

---
+++ sea-survival-game/Assets/Scripts/inventory/item-tipes/building.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "NewBuilding", menuName = "Item/Create New Building")]
+public class building : Item
+{
+    public TileBase tile;
+}

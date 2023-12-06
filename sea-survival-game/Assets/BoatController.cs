99a869278c386d2cff418ce68f4e03117fe187b8
1Tempest2 <babinszkibotond@gmail.com>
2023-12-06 19:54:53 +0100

    Boat 6.0

--- sea-survival-game/Assets/BoatController.cs
+++ sea-survival-game/Assets/BoatController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,32 +8,51 @@ using UnityEngine.UIElements;
 
 public class BoatController : MonoBehaviour
 {
-
+    enum BoatType
+    {
+        BoatLevelOne,
+        BoatLevelTwo, 
+        BoatLevelThree,
+    }
     public GameObject seat;
+    CinemachineVirtualCamera cinemachineVirtualCamera;
+    public GameObject cinemachineCamera;
     public CharacterController2D characterController;
     public UnityEngine.UI.Slider speedslider;
     public TextMeshProUGUI speedText;
     public Canvas boatUICanvas;
-    public AnimationCurve turnSpeedCurve;
+    public AnimationCurve accelCurve;
+    public AnimationCurve rotationCurve;
+    [SerializeField] BoatType boatType;
 
-    public float maxSpeed = 5f;
-    public float maxTorque = 0.1f;
-    public float torqueDecayRate = 0.075f;
-    public float rotationSpeed = 0.075f;
-    public float acceleration = 0.15f;
-    public float deceleration = 0.25f;
+    public float maxSpeed;
+    public float maxTorque;
+    public float torqueDecayRate;
+    public float rotationSpeed;
+    public float acceleration;
+    public float deceleration;
     private bool isPlayerInSeat = false;
 
     private Rigidbody2D boatrb;
 
-
     void Start()
-    {
+    { 
         boatrb = GetComponent<Rigidbody2D>();
+        cinemachineVirtualCamera = cinemachineCamera.GetComponent<CinemachineVirtualCamera>();
         speedslider.maxValue = 30;
         speedslider.minValue = 0;
         speedText.text = string.Empty;
+        if(boatType == BoatType.BoatLevelOne)
+          {
+             maxSpeed = 5f;
+             maxTorque = 1.5f;
+             torqueDecayRate = 0.075f;
+             rotationSpeed = 1.125f;
+             acceleration = 0.3f;
+             deceleration = 0.15f;
+          }
     }
+
 
     void Update()
     {
@@ -43,9 +63,8 @@ public class BoatController : MonoBehaviour
 
         if (isPlayerInSeat)
         {
-            GameManagerSingleton.Instance.player.transform.position = seat.transform.position;
+            GameManagerSingleton.Instance.player.transform.position = seat.transform.position; //megoldás majd megoldom
             HandleBoatMovement();
-            ChangeTurnFloat(boatrb.velocity.magnitude);
         }
     }
 
@@ -55,6 +74,7 @@ public class BoatController : MonoBehaviour
         {
             characterController.enabled = true;
             isPlayerInSeat = false;
+            cinemachineVirtualCamera.m_Lens.OrthographicSize = 5;
             boatUICanvas.enabled = false;
         }
         else
@@ -62,6 +82,7 @@ public class BoatController : MonoBehaviour
             isPlayerInSeat = true;
             boatUICanvas.enabled = true;
             characterController.enabled = false;
+            cinemachineVirtualCamera.m_Lens.OrthographicSize = 10;
             GameManagerSingleton.Instance.player.transform.position = seat.transform.position;
         }
     }
@@ -70,34 +91,27 @@ public class BoatController : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-
-        // Prevent moving backward
+        //ne menjen hátra
         if (verticalInput < 0)
         {
             verticalInput = 0;
         }
 
-        float torqueToApply = -horizontalInput * rotationSpeed;
+        float torqueToApply = -horizontalInput * (rotationSpeed * rotationCurve.Evaluate(boatrb.velocity.magnitude)); // úgy kanyarodj amilyen gyorsan mész
         torqueToApply = Mathf.Clamp(torqueToApply, -maxTorque, maxTorque);
         boatrb.AddTorque(torqueToApply);
 
-        // Decay torque over time
         boatrb.AddTorque(-boatrb.angularVelocity * torqueDecayRate);
 
-        Vector2 forwardVector = transform.right;
-        boatrb.AddForce(forwardVector * verticalInput * acceleration, ForceMode2D.Impulse);
-
+        Vector2 forwardVector = new Vector2(Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad));
+       // Debug.Log(accelCurve.Evaluate(boatrb.velocity.magnitude).ToString());
+        boatrb.AddForce(accelCurve.Evaluate(boatrb.velocity.magnitude) * acceleration * verticalInput * forwardVector, ForceMode2D.Impulse);
         LimitSpeed();
 
         speedslider.value = boatrb.velocity.magnitude * 6;
         speedText.text = Mathf.CeilToInt((boatrb.velocity.magnitude * 6)).ToString();
 
         boatrb.velocity *= (1f - Time.deltaTime * deceleration);
-    }
-
-    void ChangeTurnFloat(float currentVelocity)
-    {
-       //
     }
 
     void LimitSpeed()

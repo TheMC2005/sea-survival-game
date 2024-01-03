d86b7e1aab53767d3f1ea00a72d4b4a05d74d1e1
TheMC2005 <erdelyi.nimrod05@gmail.com>
2024-01-03 18:47:40 +0100

    atack4

--- sea-survival-game/Assets/Scripts/Tools/Attack.cs
+++ sea-survival-game/Assets/Scripts/Tools/Attack.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Data/Tool Action/Attack")]
 public class Attack : ToolAction
 {
-    public override bool OnApply(Vector2 worldPoint){return true;}
+    public override bool OnApply(Vector2 worldPoint){Debug.Log("fasz"); return true;}
     public override void OnItemUsed(Weapon usedItem)
     {
-        GameObject attack = Instantiate(usedItem.Attack,Aim.aim.transform);
+        Debug.Log("asd");
+        GameObject attack = Instantiate(usedItem.Attack,Aim.aim.transform.position,Aim.aim.transform.rotation);
     }
 }
